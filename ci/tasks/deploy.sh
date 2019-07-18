8845a9415388d39d54c2645cadbc4229e87041bb
Niall Thomson <nthomson@pivotal.io>
2019-07-17 20:50:28 -0700

    Working on self support

---
+++ ci/tasks/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p /root/.kube
+
+ca_path="/root/.kube/ca.pem"
+
+echo "$cluster_ca" | base64 -d > $ca_path
+
+kubectl config set-cluster default --server=$cluster_url --certificate-authority=$ca_path
