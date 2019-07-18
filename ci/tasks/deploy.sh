dbc4c216fd9f3c833a1a259caf4d1b907e77c9cb
Niall Thomson <nthomson@pivotal.io>
2019-07-17 20:56:24 -0700

    Working on self support

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -10,4 +10,8 @@ echo "$cluster_ca" | base64 -d > $ca_path
 
 kubectl config set-cluster default --server=$cluster_url --certificate-authority=$ca_path
 
+kubectl config use-context default
+
+kubectl config view
+
 kubectl get pods
